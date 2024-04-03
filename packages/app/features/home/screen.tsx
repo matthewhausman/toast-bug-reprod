82eb5b0308d1d8547255ce93e04f85e7aa4484c6
Matthew Hausman <matthewhausman7@gmail.com>
2024-04-03 14:55:06 -0700

    upd

--- packages/app/features/home/screen.tsx
+++ packages/app/features/home/screen.tsx
@@ -14,42 +14,15 @@ import {
   useToastController,
   validToken,
 } from '@my/ui'
-import { ArrowRight, DollarSign, Pencil, User, Users } from '@tamagui/lucide-icons'
 import React, { useEffect, useState } from 'react'
 import { Platform } from 'react-native'
-import { useLink } from 'solito/link'
-import { useRouter } from 'solito/router'
-
-const defaultAuthors = [
-  {
-    id: 1,
-    name: 'John Doe',
-    avatar: 'https://i.pravatar.cc/150?img=67/32/32?ca=1',
-  },
-  {
-    id: 2,
-    name: 'Jane Doe',
-    avatar: 'https://i.pravatar.cc/150?img=30/32/32?ca=1',
-  },
-]
 
 export function HomeScreen() {
-  const router = useRouter()
-  const [show, setShow] = useState(false)
-  useEffect(() => {
-    const t = setTimeout(() => {
-      setShow(true)
-    }, 5000)
-    return () => clearTimeout(t)
-  }, [])
-
   const toastController = useToastController()
-  if (!show) return null
+
   return (
     <XStack maw={1480} als="center" f={1}>
       <ScrollView f={3} fb={0}>
-        {/* <AnimatePresence> */}
-
         <YStack
           key="home-animation-test"
           animation="200ms"
@@ -62,48 +35,13 @@ export function HomeScreen() {
         >
           <Button
             onPress={() => {
-              // router.push('/about')
               toastController.show('testing', { message: 'testing' })
             }}
           >
             Show Toast
           </Button>
         </YStack>
-
-        {/* </AnimatePresence> */}
       </ScrollView>
     </XStack>
-  )
-}
-
-const halfMinusSpace = validToken(
-  Platform.select({
-    web: 'calc(50% - 12px)',
-    native: '53%',
-  })
-)
-
-const quarterMinusSpace = validToken(
-  Platform.select({
-    web: 'calc(25% - 12px)',
-    native: '21%',
-  })
-)
-
-const feedCardWidthMd = validToken(
-  Platform.select({
-    web: 'calc(33.33% - 12px)',
-    native: '32%',
-  })
-)
-
-function ScrollAdapt({ children }: { children: React.ReactNode }) {
-  const { md } = useMedia()
-  return md ? (
-    <ScrollView horizontal showsHorizontalScrollIndicator={false}>
-      {children}
-    </ScrollView>
-  ) : (
-    <>{children}</>
   )
 }
